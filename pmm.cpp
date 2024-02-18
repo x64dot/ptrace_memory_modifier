3c31a9fd0e9bb7ca5eb1da2415814311a0d136a9
x64dot <142705403+x64dot@users.noreply.github.com>
2024-02-18 11:12:47 -0500

    Update pmm.cpp
    Added command-line argument parsing for process ID, memory address, and value.

--- pmm.cpp
+++ pmm.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
+#include <cstdlib>
+#include <cstring>
 #include <sys/ptrace.h>
 #include <sys/wait.h>
 
 using namespace std;
 
+int main(int argc, char* argv[]){
+    if (argc != 4){
+        cout << "Usage: " << argv[0] << " <process_id> <memory_address> <value>\n";
+        return -1;
+    }
 
-int main(){
-    pid_t process_id = 0; /* Enter process_id of the running process */
-    uintptr_t memory_address = 0; /* Enter the memory address of the value you want to change */
-    long value = 0; /* Enter the new value */
+    pid_t process_id = atoi(argv[1]);
+    uintptr_t memory_address = strtol(argv[2], NULL, 16);
+    long value = strtol(argv[3], NULL, 10);
 
     long ps1 = ptrace(PTRACE_ATTACH, process_id, NULL, NULL);
 
     if (ps1 == -1){
         cout << "[!] Couldn't attach to the process.\n";
-
         return -1;
     }
-    
 
     cout << "[+] Attached to the process!\n";
 
@@ -28,13 +32,12 @@ int main(){
     if (ps2 == -1){
         cout << "[!] Couldnt modify value!\n";
         ptrace(PTRACE_DETACH, process_id, NULL, NULL);
-
         return -1;
     }
 
     cout << "[+] Value Modified!\n";
 
     ptrace(PTRACE_DETACH, process_id, NULL, NULL);
-    
+
     return 0;
 }
